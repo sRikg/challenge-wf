657010b92cc3789e20a6c09f28ddb54d1dad3707
Srik Gorthy <sskanthgorthy@gmail.com>
2022-12-20 17:38:17 -0600

    Create slide22.sql

---
+++ slide22.sql
@@ -0,0 +1,21 @@
+SELECT
+ name, sum(transaction_amt)
+FROM
+   `bigquery-public-data.fec.indiv20`
+WHERE
+ amndt_ind = "N"
+ AND occupation IS NOT NULL
+ AND occupation NOT LIKE '%, INC%'
+ AND occupation NOT LIKE '%, LLC%'
+ AND occupation NOT LIKE '%COST CENTER%'
+ AND entity_tp = 'IND'
+GROUP BY name
+HAVING
+ name IS NOT NULL
+ AND name LIKE '%, %'
+ AND name NOT LIKE '%, INC%'
+ AND name NOT LIKE '%, .%'
+ AND name NOT LIKE '%, ASSOCIATION%'
+ AND name NOT LIKE '%#N/A%'
+ORDER BY 2 DESC
+LIMIT 20;
